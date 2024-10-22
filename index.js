a6b92daf0d3ccba4c788c75020c9991ede0f36ac
strna98 <yongleksitissu@gmail.com>
2024-10-22 17:32:03 +0700

    gantijs

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const gantinama = () => {
     const revi = document.getElementsByClassName("revi")
     revi[0].innerHTML = "SANGAT TANGGUH!"
 }
+
+const gantip = () => {
+    const vu = document.getElementsByClassName("vu")
+    vu[0].innerHTML = "SANGAT WOW!"
+}
